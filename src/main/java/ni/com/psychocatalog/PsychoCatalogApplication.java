f974375334bb106deba91e11040455d9e60f73eb
Jorge Luis Morales Reyes <jorge.morales@uam.edu.ni>
2026-04-28 15:06:43 -0600

    primer commit

---
+++ src/main/java/ni/com/psychocatalog/PsychoCatalogApplication.java
@@ -0,0 +1,13 @@
+package ni.com.psychocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychoCatalogApplication.class, args);
+    }
+
+}
